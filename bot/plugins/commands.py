458687eedfad128593938b9d6f0167566ba1289f
godgroot <85842733+godgroot@users.noreply.github.com>
2021-07-14 21:07:48 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = @cinemazip",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🤖 our Group 🤖', url="https://t.me/cinemazip"
                                 )
                         ]
                     ]
